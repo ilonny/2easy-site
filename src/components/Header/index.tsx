fcd61001f014fa26084960cb384e0c74bc728697
Dmitry Gornak <>
2024-10-29 12:08:55 +0300

    site content wrapper

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,30 +7,39 @@ import { AuthContext } from "@/auth";
 import { Button } from "@/ui/Button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ContentWrapper } from "../ContentWrapper";
 export const Header = () => {
   const pathname = usePathname();
-  const { profile } = useContext(AuthContext);
+  const { profile, profileIsLoading } = useContext(AuthContext);
   if (pathname === "/login") {
     return null;
   }
-
+  console.log("profileIsLoading", profileIsLoading, profile)
   return (
-    <div className="flex flex-row justify-between border-b-[1px] border-[#D9D9D9] mx-8 py-8">
-      <div className="left"></div>
-      <a href="/">
-        <div className="center absolute top-30 left-1/2 -ml-[53px]">
-          <Image priority={false} src={Logo} alt="logo" />
-        </div>
-      </a>
-      <div className="right">
-        {profile?.name ? (
-          <HeaderProfile />
+    <ContentWrapper>
+      <div className="flex flex-row justify-between border-b-[1px] border-[#D9D9D9] mx-8 py-8">
+        <div className="left"></div>
+        <a href="/">
+          <div className="center absolute top-30 left-1/2 -ml-[53px]">
+            <Image priority={false} src={Logo} alt="logo" />
+          </div>
+        </a>
+        {profileIsLoading ? (
+          <div>
+            <>loading</>
+          </div>
         ) : (
-          <Link href="/login">
-            <Button text="Войти" kind="secondary" />
-          </Link>
+          <div className="right">
+            {profile?.name ? (
+              <HeaderProfile />
+            ) : (
+              <Link href="/login">
+                <Button text="Войти" kind="secondary" />
+              </Link>
+            )}
+          </div>
         )}
       </div>
-    </div>
+    </ContentWrapper>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./Header";
 export { HeaderProfile } from "./HeaderProfile";
+export { ContentWrapper } from "./ContentWrapper";
