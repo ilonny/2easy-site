0f00d20696a21477c1f003bc48c4a2022f096728
Dmitry Gornak <dgornak@ecom.tech>
2025-07-01 12:12:15 +0300

    past simple

---
+++ src/app/grammar/past_simple/data.ts
@@ -0,0 +1,14 @@
+import { TSquare } from "@/components/SquareList";
+import Img1 from "@/assets/images/grammar/main_blocks/past_simple_1.svg";
+import Img2 from "@/assets/images/grammar/main_blocks/past_simple_2.svg";
+
+export const data: TSquare[] = [
+  {
+    link: "/grammar/past_simple/irregular_verbs",
+    bgImage: Img1,
+  },
+  {
+    link: "/grammar/past_simple/real_life_stories",
+    bgImage: Img2,
+  },
+];
