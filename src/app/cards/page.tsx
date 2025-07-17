e3df372fedc5e853c975e1c9483d5618eb70ace9
Dmitry Gornak <dgornak@ecom.tech>
2025-07-17 19:37:34 +0300

    fix 33 percent lg

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -46,7 +46,7 @@ export default function GrammarPage() {
           {data.map((el) => {
             return (
               <Link
-                className="w-[100%] lg:w-[33.333333%] p-2 mb-4"
+                className="w-[100%] lg:w-[33.33333%] p-2 mb-4"
                 key={el.id}
                 href={el.link}
               >
