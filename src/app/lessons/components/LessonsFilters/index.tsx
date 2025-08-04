c26183ed570c7bf55dfb34372cced7cb60e43562
Dmitry Gornak <dgornak@ecom.tech>
2025-08-04 19:28:14 +0300

    714201

---
+++ src/app/lessons/components/LessonsFilters/index.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "@nextui-org/react";
+import { FC, useMemo } from "react";
+
+type TProps = {
+  tabIndex: "userLessons" | "savedLessons";
+};
+
+export const LessonsFilters: FC<TProps> = ({ tabIndex }) => {
+  return <></>;
+};
--- src/app/lessons/components/ProfileLessons/index.tsx
+++ src/app/lessons/components/ProfileLessons/index.tsx
@@ -1,15 +1,18 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input, Tab, Tabs, Image } from "@nextui-org/react";
 import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { useLessons } from "../../hooks/useLessons";
 import { ProfileEmptyLessons } from "../ProfileEmptyLessons";
 import { CreateLessonModalForm } from "../CreateLessonModalForm";
 import { LessonsList } from "../LessonsList";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import Dino from "@/assets/images/dino.gif";
 import { useCheckSubscription } from "@/app/subscription/helpers";
 import { AuthContext } from "@/auth";
 import { readFromLocalStorage, writeToLocalStorage } from "@/auth/utils";
+import { LessonsFilters } from "../LessonsFilters";
+import { tabs } from "../LessonsFilters/tabs";
+import Loupe from "@/assets/icons/loupe.svg";
+import { TLesson } from "../../types";
 
 type TProps = {
   canCreateLesson?: boolean;
@@ -38,6 +41,7 @@ export const ProfileLessons = (props: TProps) => {
   const router = useRouter();
   const { profile } = useContext(AuthContext);
   const isTeacher = profile?.role_id === 2;
+  const [activeFilterTab, setActiveFilterTab] = useState("");
 
   const [tabIndex, setTabIndex] = useState<"userLessons" | "savedLessons">(
     "userLessons"
@@ -87,17 +91,6 @@ export const ProfileLessons = (props: TProps) => {
 
   const { checkSubscription } = useCheckSubscription();
 
-  const lessonsToRender = useMemo(() => {
-    if (!isTeacher) {
-      return lessons;
-    }
-    if (tabIndex === "userLessons" && !studentId) {
-      return lessons.filter((l) => l.user_id !== 1);
-    }
-    // return lessons;
-    return lessons.filter((l) => l.user_id === 1);
-  }, [isTeacher, lessons, tabIndex, studentId]);
-
   useEffect(() => {
     const index = readFromLocalStorage("saved_lessons_tab");
     if (index) {
@@ -111,27 +104,135 @@ export const ProfileLessons = (props: TProps) => {
     writeToLocalStorage("saved_lessons_tab", tabIndex);
   }, [tabIndex]);
 
+  const [filterSearchString, setFilterSearchString] = useState("");
+
+  const lessonsToRender = useMemo(() => {
+    if (!isTeacher) {
+      return lessons;
+    }
+    if (tabIndex === "userLessons" && !studentId) {
+      return lessons.filter((l) => l.user_id !== 1);
+    }
+    // return lessons;
+    return lessons.filter((l) => l.user_id === 1);
+  }, [isTeacher, lessons, tabIndex, studentId]);
+
+  const tabsToRender = useMemo(() => {
+    if (tabIndex === "savedLessons") {
+      return tabs;
+    }
+    return ["All lessons"].concat(
+      Array.from(
+        new Set(
+          lessonsToRender.map((lesson) => {
+            return lesson.tags;
+          })
+        )
+      ).filter(Boolean)
+    );
+  }, [lessonsToRender, tabIndex]);
+
+  useEffect(() => {
+    if (tabsToRender.length) {
+      setActiveFilterTab(tabsToRender?.[0] || "");
+    }
+    setFilterSearchString("");
+  }, [tabsToRender, tabIndex]);
+
+  const filteredLessons = useMemo(() => {
+    let res = lessonsToRender;
+    if (activeFilterTab !== "All lessons") {
+      res = res.filter((lesson: TLesson) => {
+        if (
+          !!lesson.tags &&
+          activeFilterTab
+            ?.toLowerCase()
+            ?.includes(lesson.tags?.toLowerCase() || "")
+        ) {
+          return true;
+        }
+        return false;
+      });
+    }
+    if (filterSearchString) {
+      res = res.filter((lesson: TLesson) => {
+        if (
+          lesson.title
+            ?.toLowerCase()
+            ?.includes(filterSearchString?.toLowerCase()) ||
+          lesson.description
+            ?.toLowerCase()
+            ?.includes(filterSearchString?.toLowerCase()) ||
+          lesson.tags
+            ?.toLowerCase()
+            ?.includes(filterSearchString?.toLowerCase())
+        ) {
+          return true;
+        }
+        return false;
+      });
+    }
+    return res;
+  }, [activeFilterTab, filterSearchString, lessonsToRender]);
+
   return (
     <>
       {!hideTabs && (
-        <div className="flex gap-5 justify-center">
-          <Button
-            radius="full"
-            color="primary"
-            variant={tabIndex === "userLessons" ? "solid" : "faded"}
-            onClick={() => setTabIndex("userLessons")}
-          >
-            Мои уроки
-          </Button>
-          <Button
-            radius="full"
-            color="primary"
-            variant={tabIndex === "savedLessons" ? "solid" : "faded"}
-            onClick={() => setTabIndex("savedLessons")}
-          >
-            Уроки 2EASY
-          </Button>
-        </div>
+        <>
+          <div className="flex gap-5 justify-center">
+            <Button
+              radius="full"
+              color="primary"
+              variant={tabIndex === "userLessons" ? "solid" : "faded"}
+              onClick={() => setTabIndex("userLessons")}
+            >
+              Мои уроки
+            </Button>
+            <Button
+              radius="full"
+              color="primary"
+              variant={tabIndex === "savedLessons" ? "solid" : "faded"}
+              onClick={() => setTabIndex("savedLessons")}
+            >
+              Уроки 2EASY
+            </Button>
+          </div>
+          {tabsToRender?.length >= 2 && (
+            <>
+              <div className="h-6"></div>
+              <Tabs
+                color="primary"
+                size="lg"
+                aria-label="Tabs"
+                radius={"none"}
+                selectedKey={activeFilterTab}
+                onSelectionChange={(val) => setActiveFilterTab(val)}
+                fullWidth
+              >
+                {tabsToRender.map((t) => {
+                  return <Tab key={t} title={t} />;
+                })}
+              </Tabs>
+            </>
+          )}
+          <div className="h-6"></div>
+          <div className="w-[100%] lg:w-[525px] m-auto">
+            <Input
+              value={filterSearchString}
+              onValueChange={setFilterSearchString}
+              placeholder="Поиск уроков"
+              size="lg"
+              classNames={{ inputWrapper: "bg-white hove" }}
+              startContent={
+                <Image
+                  src={Loupe.src}
+                  alt="search"
+                  style={{ borderRadius: 0 }}
+                />
+              }
+            />
+          </div>
+        </>
       )}
       <div className="h-10" />
       {lessonsListIslLoading && (
@@ -155,7 +256,7 @@ export const ProfileLessons = (props: TProps) => {
             }
           }}
           canCreateLesson={canCreateLesson}
-          lessons={lessonsToRender}
+          lessons={filteredLessons}
           getLessons={getLessons}
           hideAttachButton={hideAttachButton}
           showChangeStatusButton={showChangeStatusButton}
