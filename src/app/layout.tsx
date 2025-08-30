4453ddf4b6a25f891da7341068003c7cf2adf757
Dmitry Gornak <dgornak@ecom.tech>
2025-08-30 13:00:17 +0300

    add beta metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,25 @@ export default function RootLayout({
             </AuthContextProvider>
           </NextUIProvider>
         </ApiProvider>
+        <Script id="metrica-script">
+          {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103952819', 'ym');
+            ym(103952819, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/103952819"
+              style={{ position: "absolute", left: -99999 }}
+              alt="metrica"
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
