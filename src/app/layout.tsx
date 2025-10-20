5622611394dc6e53720caa87913304d65db5aa3d
Dmitry Gornak <dgornak@ecom.tech>
2025-10-20 13:39:19 +0300

    vk pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,27 @@ export default function RootLayout({
             />
           </div>
         </noscript>
+        <Script id="vk-pixel">
+          {`var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3710431", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");`}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://top-fwz1.mail.ru/counter?id=3710431;js=na"
+              style={{ position: "absolute", left: -9999 }}
+              alt="Top.Mail.Ru"
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
