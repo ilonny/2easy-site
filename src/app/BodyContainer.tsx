0052fe2b241e18844e1c7b72fb62a0bc35c8fa58
Dmitry Gornak <dgornak@ecom.tech>
2026-02-25 19:14:34 +0300

    survey

--- src/app/BodyContainer.tsx
+++ src/app/BodyContainer.tsx
@@ -1,13 +1,33 @@
 "use client";
 import { AuthContext } from "@/auth";
 import { useRouter } from "next/navigation";
-import { FC, useContext, useEffect } from "react";
+import { FC, useCallback, useContext, useEffect, useState } from "react";
 import { useCheckSubscription } from "./subscription/helpers";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+} from "@nextui-org/react";
+import { readFromLocalStorage, writeToLocalStorage } from "@/auth/utils";
+import Close from "@/assets/icons/close.svg";
+import Image from "next/image";
 
 export const BodyContainer: FC<any> = ({ children }) => {
   const { profile } = useContext(AuthContext);
   const router = useRouter();
   const { checkSubscription, subscription } = useCheckSubscription();
+  const [isShowSurvey, setSurvey] = useState(
+    !readFromLocalStorage("isSurveyShowed"),
+  );
+
+  const closeSurvey = useCallback(() => {
+    writeToLocalStorage("isSurveyShowed", "1");
+    setSurvey(false);
+  }, []);
+
+  const [surveyModal, setSurveyModal] = useState(false);
 
   useEffect(() => {
     // Логика перенаправления для студентов
@@ -21,49 +41,65 @@ export const BodyContainer: FC<any> = ({ children }) => {
     ) {
       router.push(`/student-account/${profile.studentId}`);
     }
+  }, [profile?.isStudent, profile.studentId, router]);
 
-    // Вставка скрипта WebAsk при условии subscription?.subscribe_type_id !== 1
-    if (subscription && subscription?.subscribe_type_id !== 1) {
-      // Проверяем, не загружен ли скрипт уже
-      if (!window.WebAsk) {
-        (function (s, u, r, v, e, y) {
-          s[u] = s[u] || r;
-          s[u].b.push({
-            id: ["a46253", "8j5h7hbg4"],
-            type: "widget",
-            mode: "bottom",
-            position: "left",
-            customLaunch: true,
-            launchEvent: "onload",
-            launchDelay: 0,
-            preventReopenOnClose: true,
-            closeOnSubmit: true,
-            panelWidth: 360,
-            panelHeight: 567,
-            buttonText: "Поделитесь своим мнением",
-            buttonColor: "rgba(39, 116, 248, 1)",
-            buttonTextColor: "rgba(255, 255, 255, 1)",
-            flags: ["stopLead", "stopMobile"],
-          });
-          v.async = true;
-          v.src = e + y;
-          document.querySelector("head").appendChild(v);
-        })(
-          window,
-          "WebAsk",
-          { b: [] },
-          document.createElement("script"),
-          "https:",
-          "//app.webask.io/static/api-v2.js",
-        );
-      }
-    }
-  }, [
-    profile?.isStudent,
-    profile.studentId,
-    router,
-    subscription?.subscribe_type_id, // Добавляем в зависимости для отслеживания изменений
-  ]);
-
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      {subscription && subscription?.subscribe_type_id && isShowSurvey && (
+        <div
+          style={{
+            position: "fixed",
+            bottom: 30,
+            left: 30,
+          }}
+        >
+          <Button onClick={() => setSurveyModal(true)} color="primary">
+            Открыть опрос
+          </Button>
+          <Image
+            onClick={(e) => {
+              e.stopPropagation();
+              closeSurvey();
+            }}
+            priority={false}
+            src={Close}
+            alt="close"
+            style={{
+              flexShrink: 0,
+              position: "absolute",
+              right: -10,
+              top: -10,
+              cursor: "pointer",
+            }}
+          />
+        </div>
+      )}
+      <Modal
+        size="4xl"
+        isOpen={surveyModal}
+        onClose={() => {
+          setSurveyModal(false);
+          closeSurvey();
+        }}
+        scrollBehavior="outside"
+      >
+        <ModalContent>
+          <ModalHeader className="justify-center">
+            <p className="text-center">Поделитесь вашим мнением</p>
+          </ModalHeader>
+          <ModalBody>
+            <div className="flex flex-wrap justify-start">
+              <iframe
+                width="100%"
+                height="600px"
+                src="https://a46253.webask.io/8j5h7hbg4"
+              ></iframe>
+            </div>
+            <div className="h-4" />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
