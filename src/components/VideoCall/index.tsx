f5beec15d09c45a8a59db08ef3f5f9101f87fe53
Dmitry Gornak <dgornak@ecom.tech>
2026-03-30 18:43:36 +0300

    mobile video

--- src/components/VideoCall/index.tsx
+++ src/components/VideoCall/index.tsx
@@ -25,7 +25,7 @@ type TProps = {
   lessonId: string | string[];
 };
 
-const JITSI_DOMAIN = "meet.evolix.org";
+const JITSI_DOMAIN = "meet.ffmuc.net";
 const MIN_W = 280;
 const MIN_H = 200;
 const MAX_W = 600;
